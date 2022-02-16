d6836f111af887ebcc2a72f8623c777cefff31b9
Art <artem_chernenko@ro.ru>
2022-02-16 10:40:43 +0300

    Initial Commit

---
+++ TrafficLighter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrafficLighter
+//
+//  Created by Артем Черненко on 16.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrafficLighter/TrafficLighterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLighterApp.swift
+//  TrafficLighter
+//
+//  Created by Артем Черненко on 16.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLighterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
